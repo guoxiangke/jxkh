4e4071ddb80d6a7e4325d60f2ab5bf6e681b77d0
xiangkeguo@gmail.com <xiangkeguo@gmail.com>
2013-11-01 19:09:51 +0800

    git 1101

--- sites/all/modules/sbq_commons/js/sbq-commons.js
+++ sites/all/modules/sbq_commons/js/sbq-commons.js
@@ -1,33 +1,51 @@
 jQuery(function($) {
-  $('.alert-block a.close').click(function(){
-    $(this).parents('.alert-block').hide();
-  });		
-  $('.node-type-sbq-topic textarea').focus(function(){
-    $(this).attr('value','');
-  });
-  $('.user_doctor_register_ajax').click(function(){
-    var href = $(this).attr('href');
-    if( $('#sbq_doctor_quick_register').length > 0){
-      $('#sbq_doctor_quick_register').modal();
-    }else{
-      var self = $(this);
-      self.parent().parent().find("li.first").append('<div id="status" style="background: url(/misc/throbber.gif) no-repeat 5px -16px; width:21px; height:16px;display: inline-block;"> </div>');
-      $.get(href+'?ajax=true', function(data){ 
-        self.parent().parent().find("li #status").remove()
-        $(data).modal();
-      });
-    }
-    return false;
-  });
-  Drupal.behaviors.sbq_commons = {
-    attach: function (context, settings) {
-      $('.alert-block a.close').click(function(){
+    $('.alert-block a.close').click(function() {
         $(this).parents('.alert-block').hide();
-      });
-				
-      $('.node-type-sbq-topic textarea').focus(function(){
-        $(this).attr('value','');
-      });
+    });
+    $('.node-type-sbq-topic textarea').focus(function() {
+        $(this).attr('value', '');
+    });
+    $('.user_doctor_register_ajax').click(function() {
+        var href = $(this).attr('href');
+        if ($('#sbq_doctor_quick_register').length > 0) {
+            $('#sbq_doctor_quick_register').modal();
+        } else {
+            var self = $(this);
+            self.parent().parent().find("li.first").append('<div id="status" style="background: url(/misc/throbber.gif) no-repeat 5px -16px; width:21px; height:16px;display: inline-block;"> </div>');
+            $.get(href + '?ajax=true', function(data) {
+                self.parent().parent().find("li #status").remove()
+                $(data).modal();
+            });
+        }
+        return false;
+    })
+    // 注册同意服务条款
+    $('#user-register-form #edit-submit').live('click', function() {
+        if ($('#user-register-form #edit-agree').is(':checked')) {
+            return true;
+        }
+        alert('请同意伤不起服务条款');
+        return  false;
+    })
+    
+    // 用户关系请求
+    $('.relationship_ajax_action').live('click', function(){
+        var self = $(this);
+         self.append('<div id="status" style="background: url(/misc/throbber.gif) no-repeat 5px -16px; width:21px; height:16px;display: inline-block;"> </div>');
+        $.getJSON(self.attr('href')+'?ajax=true',function(data){
+           $('#sbq_commons_user_relationship_view').replaceWith(data['html']);
+        })
+        return false;
+    })
+    Drupal.behaviors.sbq_commons = {
+        attach: function(context, settings) {
+            $('.alert-block a.close').click(function() {
+                $(this).parents('.alert-block').hide();
+            });
+
+            $('.node-type-sbq-topic textarea').focus(function() {
+                $(this).attr('value', '');
+            });
+        }
     }
-  }
 });
