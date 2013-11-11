ceb0195b7bfd5fbfa8ccb8dcd801b8dd42dfea47
guo <guo@ylnwt.com>
2013-11-11 11:37:04 +0800

    top nav

--- sites/all/themes/zen_kh/templates/page.tpl.php
+++ sites/all/themes/zen_kh/templates/page.tpl.php
@@ -8,6 +8,21 @@
  */
 ?>
 
+<?php if ($secondary_menu): ?>
+  <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+    <?php print theme('links__system_secondary_menu', array(
+      'links' => $secondary_menu,
+      'attributes' => array(
+        'class' => array('links', 'inline', 'clearfix'),
+      ),
+      'heading' => array(
+        'text' => $secondary_menu_heading,
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    )); ?>
+  </nav>
+<?php endif; ?>
 <header class="header" id="header" role="banner">
   <div class="sbq-center sbq-width sbq-clearfix">
     <?php if ($logo): ?>
@@ -27,23 +42,6 @@
         <?php endif; ?>
       </div>
     <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
 
     <div id="navigation">
 
