a6212bda7357fbb0e930d60e699c1bb555047260
guo <guo@ylnwt.com>
2013-11-11 15:12:35 +0800

    add weight for annoylin

--- sites/all/themes/zen_kh/template.php
+++ sites/all/themes/zen_kh/template.php
@@ -162,7 +162,7 @@ function zen_kh_form_alter(&$form, &$form_state, $form_id) {
     if (variable_get('user_register', USER_REGISTER_VISITORS_ADMINISTRATIVE_APPROVAL)) {
       $items[] = l(t('Visit anonymous'), 'center', array('attributes' => array('title' => t('Create a new user account.'))));
     }
-    $form['anonymous_link'] = array('#markup' => theme('item_list', array('items' => $items, 'attributes' => array('class' => 'sbq_anonymous_visit_button'))), '#weight' => 10);
+    $form['anonymous_link'] = array('#markup' => theme('item_list', array('items' => $items, 'attributes' => array('class' => 'sbq_anonymous_visit_button'))), '#weight' => 10000);
 
     $form['actions']['#weight'] = 6;
     $form['links']['#weight'] = 7;
