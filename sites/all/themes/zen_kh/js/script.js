01e8cd3eec827a919cfc34e44219ce38c0df5009
guo <guo@ylnwt.com>
2013-11-19 16:57:36 +0800

    1119-02

--- sites/all/themes/zen_kh/js/script.js
+++ sites/all/themes/zen_kh/js/script.js
@@ -34,6 +34,41 @@ Drupal.behaviors.my_custom_behavior = {
     $('#sbq_doctor_quick_register button.close').live('click',function(){
     	$('#sbq_doctor_quick_register').hide();
     })
+    /*我的帖子*/
+    $('#block-sbq-commons-sbq-user-menu .sbq-user-menu-post a').click(function() {
+      var ttop = $('#block-quicktabs-user-group').offset().top-30;
+      $('html,body').animate({ scrollTop: ttop }, 1000);
+      return false;
+    });
+    /*我的动态*/
+    $('#block-sbq-commons-sbq-user-menu .sbq-user-menu-status a').click(function() {
+      var btop = $('#block-quicktabs-message').offset().top-30;
+      $('html,body').animate({ scrollTop: btop }, 1000);
+      return false;
+    });
+    $('.view-og-user-groups .views-row,.view-og-extras-groups .views-row').hover(function(){
+      $(this).children('.views-field-body').show();
+    },function(){
+      $(this).children('.views-field-body').hide();
+    })
+
+    /*个人中心提交问题*/
+    $('#edit-submit--2').click(function(event) {
+      /* Act on the event */
+      var qs = $.trim($('#edit-body-und-0-value').val());
+      if(qs){
+        var today = new Date();
+        var todayString = today.getFullYear() +'-'+today.getMonth() + '-' + today.getDate();
+        var nqs  = '<div class="views-row views-row-1 views-row-odd views-row-first">';
+            nqs += '<div class="views-field views-field-title"><span class="field-content">'+qs+'</span>  </div>';  
+            nqs += '  <div class="views-field views-field-value">        <span class="field-content">0</span>  </div> '; 
+            nqs += '  <div class="views-field views-field-field-mark-question-resolved">        <div class="field-content">未解决</div>  </div>  ';
+            nqs += '  <div class="views-field views-field-created">        <span class="field-content">'+todayString+'</span>  </div>  </div>';
+
+        $('#quicktabs-tabpage-user_qa-0 .view-content').prepend(nqs)
+      }
+      return false;
+    });
   }
 };
 
