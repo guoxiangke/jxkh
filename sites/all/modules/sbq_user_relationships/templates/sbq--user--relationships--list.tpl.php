ad01bb4fafcd4469bfcd4727673463e2c046fa75
CHECHAO <wow.che@gmail.com>
2013-11-26 15:55:58 +0800

    update sbq_user_relationships add user relationship ajax action

--- sites/all/modules/sbq_user_relationships/templates/sbq--user--relationships--list.tpl.php
+++ sites/all/modules/sbq_user_relationships/templates/sbq--user--relationships--list.tpl.php
@@ -14,6 +14,8 @@
     </div>
   <?php endif; ?>
   <?php foreach ($accounts as $account) : ?>
-    <?php print sbq_user_relationships_profile($account); ?>
+    <?php 
+    print sbq_user_relationships_profile($account); 
+    ?>
   <?php endforeach; ?>
 </div>
