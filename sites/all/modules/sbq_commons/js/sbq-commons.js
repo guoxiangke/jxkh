cc0ec978cd34509d386af49141fbb471aa105c22
CHECHAO <wow.che@gmail.com>
2013-12-02 18:16:32 +0800

    alter quick doctor login

--- sites/all/modules/sbq_commons/js/sbq-commons.js
+++ sites/all/modules/sbq_commons/js/sbq-commons.js
@@ -10,15 +10,16 @@ jQuery(function($) {
     $('#sbq-user-disease-add-submit').live("click", function() {
         var tag_name = $('#sbq-user-disease-add').val();
         if (tag_name != '' && tag_name != '回车添加标签') {
-          var add_url = '/user/ajax/tags/'+tag_name+'/add';
-          $.ajax({
-            url: add_url
-          }).done(function(result) {
-            var insert_li = result[1].data;
-            $(insert_li).appendTo($('.views-field-field-tags-disease ul'));
-          });
-          return false;
-        };
+            var add_url = '/user/ajax/tags/' + tag_name + '/add';
+            $.ajax({
+                url: add_url
+            }).done(function(result) {
+                var insert_li = result[1].data;
+                $(insert_li).appendTo($('.views-field-field-tags-disease ul'));
+            });
+            return false;
+        }
+        ;
     });
 
     // User Center tag delete
@@ -26,26 +27,46 @@ jQuery(function($) {
         var del_url = $(this).attr('href');
         var parent_li = $(this).parents('li');
         $.ajax({
-          url: del_url
+            url: del_url
         }).done(function() {
-          parent_li.hide();
+            parent_li.hide();
         });
         return false;
     });
+//    $('.user_doctor_register_ajax').click(function() {
+//        alert('aa');
+//        var href = $(this).attr('href');
+//        if ($('#sbq_doctor_quick_register').length > 0) {
+//             $('#sbq_doctor_quick_register').show();
+//        } else {
+//            var self = $(this);
+//            self.parent().parent().find("li.first").append('<div id="status" style="background: url(/misc/throbber.gif) no-repeat 5px -16px; width:21px; height:16px;display: inline-block;"> </div>');
+//            $.get(href + '?ajax=true', function(data) {
+//                self.parent().parent().find("li #status").remove()
+//               // $('body').append(data);
+//                $('section #block-user-login').clone().appendTo('#sbq_doctor_quick_register .modal-body');
+//              //$('#sbq_doctor_quick_register #user-login-form div.item-list:first').remove();
+////                $('#sbq_doctor_quick_register').hide();
+//                $(data).modal();
+//            });
+//        }
+//        return false;
+//    })
+//    
     $('.user_doctor_register_ajax').click(function() {
         var href = $(this).attr('href');
         if ($('#sbq_doctor_quick_register').length > 0) {
-             $('#sbq_doctor_quick_register').show();
+            $('#sbq_doctor_quick_register').show();
         } else {
             var self = $(this);
             self.parent().parent().find("li.first").append('<div id="status" style="background: url(/misc/throbber.gif) no-repeat 5px -16px; width:21px; height:16px;display: inline-block;"> </div>');
             $.get(href + '?ajax=true', function(data) {
                 self.parent().parent().find("li #status").remove()
-//                $('body').append(data);
-//                $('section #block-user-login').clone().appendTo('#sbq_doctor_quick_register .modal-body');
+                $('body').append(data);
+                $('section #block-user-login').clone().appendTo('#sbq_doctor_quick_register .modal-body');
                 $('#sbq_doctor_quick_register #user-login-form div.item-list:first').remove();
-//                $('#sbq_doctor_quick_register').hide();
-                $(data).modal();
+                // $('#sbq_doctor_quick_register').hide();
+                //  $(data).modal();
             });
         }
         return false;
