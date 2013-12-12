0366b23668b0524475aa6ee035abbb2525c029e2
guomingxuan <guomingxuan@ylnwt.net>
2013-12-12 19:08:04 +0800

    new theme

--- sites/all/themes/zen_center/templates/html.tpl.php
+++ sites/all/themes/zen_center/templates/html.tpl.php
@@ -7,6 +7,7 @@
  * @see https://drupal.org/node/1728208
  */
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
     <?php print $head; ?>
