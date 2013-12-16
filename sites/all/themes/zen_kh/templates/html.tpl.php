2052756ee7462abf4e4e6647225e89b56c3da4d1
CHECHAO <wow.che@gmail.com>
2013-12-16 15:01:44 +0800

    remove html.tpl extra html

--- sites/all/themes/zen_kh/templates/html.tpl.php
+++ sites/all/themes/zen_kh/templates/html.tpl.php
@@ -49,68 +49,6 @@
     <?php print $page_top; ?>
     <?php print $page; ?>
     <?php print $page_bottom; ?>
-    <?php //if (!$user->uid) { ?>
-      <!-- Modal sbq_customer_register_form_modal -->
-      <!--<div class="modal fade" id="sbq_customer_register_form_modal" tabindex="-1" role="dialog" aria-labelledby="sbq_customer_register_form_modal_label" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h4 class="modal-title" id="sbq_customer_register_form_modal_label">普通注册</h4>
-            </div>
-            <div class="modal-body">
-              <?php //print $customer_register_form; ?>
-            </div>
-          </div>--><!-- /.modal-content -->
-        <!--</div>--><!-- /.modal-dialog -->
-      <!--</div>--><!-- /.modal -->
-      <!-- Modal sbq_doctor_register_form_modal -->
-      <!--<div class="modal fade" id="sbq_doctor_register_form_modal" tabindex="-1" role="dialog" aria-labelledby="sbq_doctor_register_form_modal_label" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h4 class="modal-title" id="sbq_doctor_register_form_modal_label">医生注册</h4>
-            </div>
-            <div class="modal-body">
-              <?php //print $doctor_register_form; ?>
-            </div>
-          </div>--><!-- /.modal-content -->
-        <!--</div>--><!-- /.modal-dialog -->
-      <!--</div>--><!-- /.modal -->
-      <!-- Modal sbq_user_login_form_modal -->
-<!--      <div class="modal fade" id="sbq_user_login_form_modal" tabindex="-1" role="dialog" aria-labelledby="sbq_user_login_form_modal_label" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h4 class="modal-title" id="sbq_user_login_form_modal_label">用户登录</h4>
-            </div>
-            <div class="modal-body">
-              <?php //print $user_login_form; ?>
-            </div>
-          </div> /.modal-content
-        </div> /.modal-dialog
-      </div> /.modal -->
-      <!-- Modal sbq_doctor_quick_login -->
-<!--      <div class="modal fade" id="sbq_doctor_quick_login_modal" tabindex="-1" role="dialog" aria-labelledby="sbq_user_login_form_modal_label" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h4 class="modal-title" id="sbq_user_login_form_modal_label">医生快速通道</h4>
-            </div>
-            <div class="modal-body">
-              <div class="quick-user-login">
-                <?php //print $user_login_block; ?>
-              </div>
-              <div class="quick-doctor-login">
-                 <?php //print $doctor_quick_login_form; ?>
-              </div>
-            </div>
-          </div> /.modal-content
-        </div> /.modal-dialog
-      </div> /.modal -->
-    <?php //} ?>
+ 
   </body>
 </html>
