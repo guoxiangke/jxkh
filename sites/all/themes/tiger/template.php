adb4df47022fb3c8c534f2c8b04be5bcd91e3244
guomingxuan <guomingxuan@ylnwt.net>
2013-12-18 15:00:08 +0800

    prepare to load css per page

--- sites/all/themes/tiger/template.php
+++ sites/all/themes/tiger/template.php
@@ -3,3 +3,14 @@
 /**
  * @file template.php
  */
+
+/**
+ * hook_preprocess_node
+ *
+ */
+function tiger_preprocess_node($variables) {
+  // /* can be used to load css per page, based on path, node-type, or others. */
+  // if (drupal_get_path_alias("node/{$variables['#node']->nid}") == 'foo') {
+  //   drupal_add_css(drupal_get_path('theme', 'tiger') . "/css/foo.css");
+  // }
+}
