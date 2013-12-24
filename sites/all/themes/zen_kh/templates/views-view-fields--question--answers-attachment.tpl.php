47cc0173d94e0d51223ee8eabc91ea193c6c349e
guomingxuan <guomingxuan@ylnwt.net>
2013-12-24 16:23:26 +0800

    question--answers remove editor

--- sites/all/themes/zen_kh/templates/views-view-fields--question--answers-attachment.tpl.php
+++ sites/all/themes/zen_kh/templates/views-view-fields--question--answers-attachment.tpl.php
@@ -81,21 +81,8 @@ If the variable contains markup, edit the View, go to "FORMAT", "Show:" and clic
 					<?php
           }else{
             //show who edit ,who create.
-            //$editor = user_load($node->revision_uid);
-            $editor = user_load($node->uid);
              ?>
-					<div class="author-item q-author q-edit pull-left">
-						<?php
-              $variables = array(
-                'style_name' => 'profile_small',
-                'path' =>$editor->picture->uri,
 
-              );
-              print theme_image_style($variables); ?>
-						<div class="commit pull-left">
-							<div class="timestamp"><span class="create">Edited by <?php print l(format_username($editor),'user/'.$editor->uid); ?></span><!--<span><?php //print format_date($node->revision_timestamp, 'sbq_date_medium_revert'); ?></span>--></div>
-						</div>
-					</div>
 					<div class="author-item q-author pull-left"> <?php print $picture; ?>
 						<div class="commit pull-left">
 							<!--                 <div class="timestamp"><span class="create">Create</span><?php print $created; ?></div>
