40b9a4a4955aaf33f529fecc1b014a2df19582ee
CHECHAOWORK <chechao@ylnwt.net>
2014-01-21 11:38:54 +0800

    phone interface update date is diplay

--- sites/all/modules/custom/sbq_center_reservation/templates/calendar-month-col.tpl.php
+++ sites/all/modules/custom/sbq_center_reservation/templates/calendar-month-col.tpl.php
@@ -9,7 +9,7 @@ $id = (isset($item['id'])) ? 'id="' . $item['id'] . '" ' : '';
 $date = (isset($item['date'])) ? ' data-date="' . $item['date'] . '" ' : '';
 $day = (isset($item['day_of_month'])) ? ' data-day-of-month="' . $item['day_of_month'] . '" ' : '';
 $headers = (isset($item['header_id'])) ? ' headers="' . $item['header_id'] . '" ' : '';
-$is_reservation = sbq_center_reservation_can_reservation($item['date']);
+$is_reservation = sbq_center_reservation_can_reservation($item['date'], '', TRUE);
 ?>
 <td <?php print $id ?>class="<?php print $item['class'] ?>" colspan="<?php print $item['colspan'] ?>" rowspan="<?php print $item['rowspan'] ?>"<?php print $date . $headers . $day; ?>>
 
