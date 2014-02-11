bd2fc4bcfadde1d8aa6fcc82a28edaffc598b196
guomingxuan <guomingxuan@ylnwt.net>
2014-02-11 19:10:29 +0800

    add ask link

--- sites/all/themes/tiger/templates/flag--follow.tpl.php
+++ sites/all/themes/tiger/templates/flag--follow.tpl.php
@@ -43,6 +43,7 @@
 <div class="sbq_follow_btn <?php print $flag_wrapper_classes; ?>">
   <?php if ($link_href): ?>
     <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a>
+    <a href="/node/add/question" title="我要提问" class="sbq_ask" rel="nofollow">我要提问</a>
   <?php else: ?>
     <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
   <?php endif; ?>
