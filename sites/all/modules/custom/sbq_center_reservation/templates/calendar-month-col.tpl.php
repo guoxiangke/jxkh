57693e8c1ac2a964561c6a88ed6fd9625fde420e
CHECHAOWORK <chechao@ylnwt.net>
2014-03-05 19:28:31 +0800

    center reservation add numofday control

--- sites/all/modules/custom/sbq_center_reservation/templates/calendar-month-col.tpl.php
+++ sites/all/modules/custom/sbq_center_reservation/templates/calendar-month-col.tpl.php
@@ -10,8 +10,8 @@ $date = (isset($item['date'])) ? ' data-date="' . $item['date'] . '" ' : '';
 $day = (isset($item['day_of_month'])) ? ' data-day-of-month="' . $item['day_of_month'] . '" ' : '';
 $headers = (isset($item['header_id'])) ? ' headers="' . $item['header_id'] . '" ' : '';
 if (arg(0) == 'center') {
-  $is_reservation = sbq_center_reservation_can_reservation($item['date'], '', 'day');
-  $is_display = $is_reservation['am'] || $is_reservation['pm'];
+  $is_current_month = sbq_center_reservation_can_reservation($item['date'], '', 'day');
+
   $display_item = $item['class'] != 'single-day no-entry future' && $item['class'] != 'single-day no-entry past' && $item['class'] != 'single-day no-entry today';
 }
 global $user;
@@ -19,7 +19,7 @@ global $user;
 <td <?php print $id ?>class="<?php print $item['class'] ?>" colspan="<?php print $item['colspan'] ?>" rowspan="<?php print $item['rowspan'] ?>"<?php print $date . $headers . $day; ?>>
   <?php if (arg(0) == 'center') : ?>
     <div class="inner reservation-item">
-      <?php if ($is_display): ?>
+      <?php if ($is_current_month && ($is_current_month['am']['is_reservation'] || $is_current_month['pm']['is_reservation'] )): ?>
         <div class="month day">
           <?php
           print $item['entry'];
@@ -27,41 +27,52 @@ global $user;
         </div>
         <div class="inner">
           <div class="sbq_order_actions">
+
             <dl>
+
               <dt>上午</dt>
               <dd>
-                <?php if ($is_reservation['am']): ?>
-                  <?php if ($user->uid > 0): ?>
-                    <a href="/center/<?php echo arg(1); ?>/reservation/created?date=<?php echo $item['date'] ?> 10:00" class="sbq_add">预约</a>
-                  <?php else: ?>
-                    <a href="/user/login?destination=center/<?php echo arg(1); ?>/reservation" class="sbq_add">预约</a>
-                  <?php endif; ?>
-                <?php else: ?>
-                  <a href="#" class="sbq_full">约满</a>
-                <?php endif; ?>
+                <?php
+                if ($is_current_month['am']['is_reservation']) {
+                  if ($is_current_month['am']['is_full']) {
+                    echo '  <a class="sbq_full">约满</a>';
+                  } elseif ($user->uid > 0 && (!$is_current_month['am']['is_full'])) {
+                    echo ' <a href="/center/' . arg(1) . '/reservation/created?date=' . $item['date'] . ' 10:00" class="sbq_add">预约</a>';
+                  } else {
+                    echo '<a href="/user/login?destination=center/' . arg(1) . '/reservation" class="sbq_add">预约</a>';
+                  }
+                } else {
+                  echo '停诊';
+                }
+                ?>
               </dd>
+
             </dl>
             <dl>
               <dt>下午</dt>
               <dd>
-                <?php if ($is_reservation['pm']): ?>
-                  <?php if ($user->uid > 0): ?>
-                    <a href="/center/<?php echo arg(1); ?>/reservation/created?date=<?php echo $item['date'] ?> 14:00" class="sbq_add">预约</a>
-                  <?php else: ?>
-                    <a href="/user/login?destination=center/<?php echo arg(1); ?>/reservation" class="sbq_add">预约</a>
-                  <?php endif; ?>
-                <?php else: ?>
-                  <a href="#" class="sbq_full">约满</a>
-                <?php endif; ?>
+                <?php
+                if ($is_current_month['pm']['is_reservation']) {
+                  if ($is_current_month['pm']['is_full']) {
+                    echo '  <a href="#" class="sbq_full">约满</a>';
+                  } elseif ($user->uid > 0 && (!$is_current_month['pm']['is_full'])) {
+                    echo ' <a href="/center/' . arg(1) . '/reservation/created?date=' . $item['date'] . ' 14:00" class="sbq_add">预约</a>';
+                  } else {
+                    echo '<a href="/user/login?destination=center/' . arg(1) . '/reservation" class="sbq_add">预约</a>';
+                  }
+                } else {
+                  echo '停诊';
+                }
+                ?>
               </dd>
             </dl>
           </div>
         </div>
       <?php else: ?>
         <div class="month day">
-        <?php
-        print $item['entry'];
-        ?>
+          <?php
+          print $item['entry'];
+          ?>
         </div>
       <?php endif; ?>
     </div>
