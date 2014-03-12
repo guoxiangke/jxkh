74cf61e11369229aab13f922a9d1c98a1423d545
dale-ubuntu <guoxiangke@ylnwt.net>
2014-03-12 11:44:54 +0800

    手机号码填写不正确

--- sites/all/themes/tiger/js/script.js
+++ sites/all/themes/tiger/js/script.js
@@ -321,15 +321,16 @@ Drupal.behaviors.tiger = {
    //    }
 
    // });
-
-  $('form input[name*=field_phone]').each(function(){
-    value = $(this).val();
-    var partten = /^0?1[3|4|5|8][0-9]\d{8}$/;
-    if(!partten.test(value)) {
-      alert('手机号码填写不正确!');
-    }
+  $('form [type=submit]').click(function(e){
+    $('form input[name*=field_phone]').each(function(){
+      value = $(this).val();
+      var partten = /^0?1[3|4|5|8][0-9]\d{8}$/;
+      if(!partten.test(value)) {
+        e.preventDefault();
+        alert('手机号码填写不正确!');
+      }
+    });
   });
-
 
   }
 };
